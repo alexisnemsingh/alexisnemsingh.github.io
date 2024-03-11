c73fc1e40631318286007ff73d6973ffa3f4234a
Alexis Nemsingh <alexisnemsingh@gmail.com>
2024-03-10 17:24:56 -0700

    removed for now

--- next.config.mjs
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-    images: {
-        domains: ['images.unsplash.com'],
-    output: 'export'
-    },
-};
-
-export default nextConfig;
